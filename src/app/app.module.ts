cd7e784cf1cfa0e9ea02339220696d5f3e7fcab6
Flavio Scimeca <flavioscimeca3@gmail.com>
2023-05-07 01:00:56 +0200

    installed and setup Firebase //! changed @angular/fire/compat/firstore/interfaces.d.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//*FireBase
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from 'src/environments/environment.development';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layouts/header/header.component';
@@ -35,14 +40,16 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     CommentListComponent,
     AboutUsComponent,
     PostCardComponent,
-    DashboardComponent
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyALZxX5L7miR5WxkT7NOz4KaOtFnoc_tMI',
+    authDomain: 'angular-blog-852ae.firebaseapp.com',
+    projectId: 'angular-blog-852ae',
+    storageBucket: 'angular-blog-852ae.appspot.com',
+    messagingSenderId: '911698180614',
+    appId: '1:911698180614:web:a298e1b8cb9ea892b16187',
+  },
+};
