003c7d11651fd33ee8b378beffcb36cad4db5f29
Flavio Scimeca <flavioscimeca3@gmail.com>
2023-05-07 21:23:49 +0200

    rendered categories in the option form

--- src/app/posts/new-post/new-post.component.ts
+++ src/app/posts/new-post/new-post.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CategoriesService } from 'src/app/services/categories.service';
 
 @Component({
   selector: 'app-new-post',
   templateUrl: './new-post.component.html',
   styleUrls: ['./new-post.component.scss'],
 })
-export class NewPostComponent {
+export class NewPostComponent implements OnInit {
   slugTitle!: string;
   imgSrc: any = 'assets/placeholder-image.jpg';
   selectedImg!: any;
+
+  categories!: any;
+
+  constructor(private categoryService: CategoriesService) {}
+
+  ngOnInit(): void {
+    this.categoryService.loadData().subscribe((val) => {
+      this.categories = val;
+    });
+  }
 
   generateSlug(event: any) {
     const title: string = event.target.value;
