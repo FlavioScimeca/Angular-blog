fd33d9ac93e2a2673f7f48705d2e3f28f2070719
Flavio Scimeca <flavioscimeca3@gmail.com>
2023-05-07 21:16:01 +0200

    styled new-post Component and handled previewImg in the form

--- src/app/posts/new-post/new-post.component.ts
+++ src/app/posts/new-post/new-post.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-new-post',
   templateUrl: './new-post.component.html',
-  styleUrls: ['./new-post.component.scss']
+  styleUrls: ['./new-post.component.scss'],
 })
 export class NewPostComponent {
+  slugTitle!: string;
+  imgSrc: any = 'assets/placeholder-image.jpg';
+  selectedImg!: any;
 
+  generateSlug(event: any) {
+    const title: string = event.target.value;
+    const generatSlugTitle = title.replace(/\s/g, '-');
+
+    this.slugTitle = generatSlugTitle;
+  }
+
+  showPreview(event: any) {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      this.imgSrc = e.target?.result;
+    };
+
+    reader.readAsDataURL(event.target.files[0]);
+    this.selectedImg = event.target.files[0];
+  }
 }
