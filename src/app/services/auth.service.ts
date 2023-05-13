a033badcead2d14821b933ecafa7847c553c4196
Flavio Scimeca <flavioscimeca3@gmail.com>
2023-05-14 00:02:12 +0200

    completed Logout logic

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,12 +1,16 @@
+import { JsonPipe } from '@angular/common';
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  loggedIn: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+
   constructor(
     private ngAuth: AngularFireAuth,
     private toastr: ToastrService,
@@ -18,6 +22,8 @@ export class AuthService {
       .signInWithEmailAndPassword(email, password)
       .then((logRef) => {
         this.toastr.success('Logged in successfully');
+        this.loadUser();
+        this.loggedIn.next(true);
         setTimeout(() => {
           this.router.navigate(['/dashboard']);
         }, 1000);
@@ -25,5 +31,23 @@ export class AuthService {
       .catch((err) => {
         this.toastr.error(err);
       });
+  }
+
+  loadUser() {
+    this.ngAuth.authState.subscribe((user) => {
+      localStorage.setItem('user', JSON.stringify(user));
+    });
+  }
+  logout() {
+    this.ngAuth.signOut().then(() => {
+      this.toastr.info('User logged out');
+      this.loggedIn.next(false);
+      localStorage.removeItem('user');
+      this.router.navigate(['/login']);
+    });
+  }
+
+  isLogged() {
+    return this.loggedIn.asObservable();
   }
 }
